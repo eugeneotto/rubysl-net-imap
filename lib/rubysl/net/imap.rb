db5bd6d97323ab09652cc4e7a08656f03a309c09
Brian Shirai <brixen@gmail.com>
2013-12-26 14:07:40 -0800

    Added imap.rb from 1.8.7.

--- lib/rubysl/net/imap.rb
+++ lib/rubysl/net/imap.rb
@@ -1,9 +1,2 @@
-require "rubysl-net-imap/version"
-
-module RubySL
-  module Net
-    module Imap
-      # Your code goes here...
-    end
-  end
-end
+require "rubysl/net/imap/imap"
+require "rubysl/net/imap/version"
